184e237eaebd449f19f59b2433f4b5e8af99e91d
xchrisw92 <christopher.welton@austincc.edu>
2021-11-12 17:29:56 -0600

    Worked on header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
 import './App.css';
+import Header from "./Components/Header/Header";
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <Header/>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
